d07e6a92db3f5aa9e6cc8ae13b82b2eacaec59a5
NadinFedoriv <nadinfedoriv@gmail.com>
2023-05-05 11:32:25 +0300

    done

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+import Notiflix from "notiflix";
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+function fetchCountries(name) {
+    const URL = `${BASE_URL}${name}?fields=name,capital,population,flags,languages`;
+    return fetch(URL).then((resp) => {
+        if (!resp.ok) {
+            throw new Error(  Notiflix.Notify.failure('Oops, there is no country with that name'));
+        }
+        return resp.json();
+    });
+};
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+const searchCountry = evt.target.value.trim();
+
+if (!searchCountry) {
+    list.innerHTML = "";
+  info.innerHTML = "";
+    return;
+}
+fetchCountries(searchCountry)
+  .then(countries => {
+    if (countries.length > 10) {
+      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      return;
+    }
+    if (countries.length === 1) {
+      info.innerHTML = createMarkupDetail(countries);
+      list.innerHTML = "";
+      return;
+    }
+
+    list.innerHTML = createMarkup(countries);
+    info.innerHTML = "";
+  })
+.catch(error => console.log(error));
+};
+
+
+function createMarkup(arr) {
+
+return arr.map(country => `<li class="country-item">
+<img class="county-flag" src="${country.flags.svg}" alt="${country.name.official}" width="30px" height="20px">
+<span>${country.name.official}</span>      
+</li>`
+)
+.join('');
+};
+
+function createMarkupDetail(arr) {
+  console.log(arr);
+    return arr.map(country => `<div class="country-title"><img class="county-flag" src="${country.flags.svg}" alt="${country.name.official}" width="30px" height="20px"> <h2>${country.name.official}</h2></div>
+    <ul class="country-info">
+      <li><p class="info-data"><span class="info-subtitle">Capital: </span> ${country.capital}</p>
+    </li>
+    <li><p class="info-data"><span class="info-subtitle">Population: </span> ${country.population}</p>
+    </li>
+    <li><p class="info-data"><span class="info-subtitle">Languages: </span> ${Object.values(country.languages)}</p>
+    </li>
+  </ul>`
+    )
+    .join('');
+    };
+
+
+
